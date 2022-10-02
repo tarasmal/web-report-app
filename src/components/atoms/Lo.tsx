2175026a598184301278383cecfaa569a4ea215e
itjet_device <itjet.device@gmail.com>
2022-10-02 19:53:02 +0300

    feat: added Logo component

---
+++ src/components/atoms/Lo.tsx
@@ -0,0 +1,41 @@
+import React, {FC} from 'react';
+import styled from "styled-components";
+
+
+interface LogoProps{
+    src: string,
+    alt: string,
+    width?: string
+    height?: string,
+    position?: string,
+    right?: string,
+    marginBottom?: string,
+    top?: string,
+    justifySelf?: string,
+    alignSelf?: string,
+    left?: string,
+}
+
+const StyledImg = styled.img<LogoProps>`
+  width: ${props => props.width};
+  height: ${props => props.height};
+  position: ${props => props.position || 'relative'};
+  right: ${props => props.right};
+  margin-bottom: ${props => props.marginBottom};
+  top: ${props => props.top};
+  left: ${props => props.left};
+  justify-self: ${props => props.justifySelf};
+  align-self: ${props => props.alignSelf};
+`
+
+const Logo: FC <LogoProps> = ({src, alt, ...props}) => {
+    return (
+        <StyledImg
+            src={src}
+            alt={alt}
+            {...props}
+        />
+    );
+};
+
+export default Logo;
